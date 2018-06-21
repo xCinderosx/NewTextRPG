2ceae58392f4f0747886eda67aceb4dd8c5e8139
Cinderos <kasprzakprzemyslaw417@gmail.com>
2018-06-21 02:57:52 +0200

    0.02
    Ethan asset
    Camera prototype

---
+++ Assets/Scripts/Behaviour C#/CameraScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public float range;
+    float yPos, xPos;
+    Quaternion fixedRotation;
+    Vector3 Ethan;
+
+    void Awake()
+    {
+        
+
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Ethan = transform.GetChild(0).position;
+            this.transform.position = Ethan;
+            Debug.Log(GetComponentInChildren<Transform>());
+            //transform.rotation = fixedRotation;
+        }
+
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            
+            float HorizontalValue = Input.GetAxis("Mouse X");
+            float VerticalSpeed = Input.GetAxis("Mouse Y");
+            Debug.Log(HorizontalValue);
+            //transform.Rotate(VerticalSpeed, HorizontalValue, 0);
+            transform.Rotate(new Vector3(Ethan.x, HorizontalValue, Ethan.z));
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/Behaviour C#/CameraScript_1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript_1 : MonoBehaviour
+{
+
+   
+    Quaternion fixedRotation;
+
+    void Awake()
+    {
+        fixedRotation = transform.rotation;
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.rotation = fixedRotation;
+        /*float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (Input.GetMouseButton(1))
+        {
+            
+            float xPos = h * range;
+            float yPos = v * range;
+            transform.Rotate(xPos, yPos, 0);
+        }
+        */
+
+    }
+}
